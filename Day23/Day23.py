804dfef1a4673232f01c84f1cc48a9e70c231ef5
thomas3Dsys <thomas3d@gmail.com>
2024-08-26 20:07:15 -0700

    added a comment into day23.py to test git and updating files

--- Day23/Day23.py
+++ Day23/Day23.py
@@ -4,6 +4,7 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 import time
+#dummy comment for using git
 
 screen = Screen()
 screen.setup(width=600, height=600)
